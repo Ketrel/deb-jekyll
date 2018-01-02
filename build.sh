535b32b1a5d111a4b259f8ac89e020c9bfc705da
Ketrel <webmaster@krahs-emag.com>
2018-01-02 12:34:06 -0500

    Updated build script, added templates, and added cleanup script

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
-docker build -f ./Dockerfiles/Dockerfile.jekyllmain -t treldev/jekyll:main .
-docker build -f ./Dockerfiles/Dockerfile.jekylldev -t treldev/jekyll:dev .
+#!/bin/sh
+USERUID=$(id -u)
+USERGID=$(id -g)
+IMGNAME=$(hostname -s)
+
+sed 's/__USERUID__/'"${USERUID}"'/g; s/__USERGID__/'"${USERGID}"'/g; s/__IMGNAME__/'"${IMGNAME}"'/g' templates/Dockerfile.stage1.template > Dockerfiles/Dockerfile.stage1
+docker build -f ./Dockerfiles/Dockerfile.stage1 -t ${IMGNAME}/jekylldev:stage1 .
+
+if [ $USERUID -eq 0 ]; then
+    sed 's/__USERUID__/'"${USERUID}"'/g; s/__USERGID__/'"${USERGID}"'/g; s/__IMGNAME__/'"${IMGNAME}"'/g' templates/Dockerfile.stage2b.template > Dockerfiles/Dockerfile.stage2
+else
+    sed 's/__USERUID__/'"${USERUID}"'/g; s/__USERGID__/'"${USERGID}"'/g; s/__IMGNAME__/'"${IMGNAME}"'/g' templates/Dockerfile.stage2.template > Dockerfiles/Dockerfile.stage2
+fi
+
+docker build -f ./Dockerfiles/Dockerfile.stage2 -t ${IMGNAME}/jekylldev:stage2 .
+
+sed 's/__USERUID__/'"${USERUID}"'/g; s/__USERGID__/'"${USERGID}"'/g; s/__IMGNAME__/'"${IMGNAME}"'/g' templates/docker-compose.yml.template > docker-compose.yml
+
 docker-compose up -d
---
+++ cleanup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+rm Dockerfiles/Dockerfile.*
+rm docker-compose.yml
